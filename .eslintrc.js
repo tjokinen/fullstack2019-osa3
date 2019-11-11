d250cd0893b6d8ee132776e19396cfaf106cc3c6
tjokinen <tomi.w.jokinen@gmail.com>
2019-11-11 23:46:01 +0200

    new file:   .eslintignore 	new file:   .eslintrc.js 	new file:   build/precache-manifest.3ab00fe5dc73fb6aa7a6f6e7b64afd4e.js 	new file:   build/static/js/main.cf27c107.chunk.js 	new file:   build/static/js/main.cf27c107.chunk.js.map 	new file:   models/Person.js 	new file:   mongo.js

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+    'env': {
+        'es6': true,
+        'node': true
+    },
+    'extends': 'eslint:recommended',
+    'globals': {
+        'Atomics': 'readonly',
+        'SharedArrayBuffer': 'readonly'
+    },
+    'parserOptions': {
+        'ecmaVersion': 2018
+    },
+    'rules': {
+        'eqeqeq': 'error',
+        'no-trailing-spaces': "error",
+        'object-curly-spacing': [
+            'error', 'always'
+        ],
+        'arrow-spacing': [
+            'error', { 'before': true, 'after': true }
+        ],
+        'indent': [
+            'error',
+            2
+        ],
+        'linebreak-style': [
+            'error',
+            'windows'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'never'
+        ],
+        'no-console': 0
+    }
+}
---
+++ build/precache-manifest.3ab00fe5dc73fb6aa7a6f6e7b64afd4e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34bebe6f809a1fdc72c8c9ab82d672ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b29df7612effc7160847",
+    "url": "/static/js/2.1228254b.chunk.js"
+  },
+  {
+    "revision": "77bf244310c13e779df8",
+    "url": "/static/js/main.cf27c107.chunk.js"
+  },
+  {
+    "revision": "0a60ccf74c4fd02454bf",
+    "url": "/static/js/runtime-main.8f84b47d.js"
+  }
+]);
